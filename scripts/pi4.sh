53779b4f6bfd3d229db89bacd6ed9cb3ee63c6fc
Simon Loffler <simon.loffler@acmi.net.au>
2019-10-24 15:00:58 +1100

    #15 Hard-code port 8081 in the browser launcher.

--- scripts/pi4.sh
+++ scripts/pi4.sh
@@ -25,8 +25,7 @@ sleep 10
 
 # Launch chromium browser in fullscreen on that page
 SCREEN_SCALE="${SCREEN_SCALE:-default 1.0}"
-PLAYLIST_LABEL_PORT="${PLAYLIST_LABEL_PORT:-default 8081}"
-chromium-browser --app=http://localhost:$PLAYLIST_LABEL_PORT --start-fullscreen --no-sandbox --user-data-dir --kiosk --force-device-scale-factor=$SCREEN_SCALE
+chromium-browser --app=http://localhost:8081 --start-fullscreen --no-sandbox --user-data-dir --kiosk --force-device-scale-factor=$SCREEN_SCALE
 
 # For debugging
 echo "Chromium browser exited unexpectedly."
