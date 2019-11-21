f30bafec3bf67bad1e018dfc09203abc64782f48
Simon Loffler <simon.loffler@acmi.net.au>
2019-11-21 15:50:05 +1100

    Fix SCREEN_SCALE environment variable default.

--- scripts/pi4.sh
+++ scripts/pi4.sh
@@ -24,7 +24,7 @@ python3 -u -m app.main &
 sleep 10
 
 # Launch chromium browser in fullscreen on that page
-SCREEN_SCALE="${SCREEN_SCALE:-default 1.0}"
+SCREEN_SCALE="${SCREEN_SCALE:-1.0}"
 chromium-browser --app=http://localhost:8081 --start-fullscreen --no-sandbox --user-data-dir --kiosk --force-device-scale-factor=$SCREEN_SCALE
 
 # For debugging
