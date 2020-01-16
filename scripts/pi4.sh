a5de75fed2bd57d004f050262e42ddb5b06abbaf
Simon Loffler <simon.loffler@acmi.net.au>
2020-01-16 14:47:56 +1100

    Disable backing store limit in chromium, and enable logging to try and debug 'aw snap' in production when loading images.

--- scripts/pi4.sh
+++ scripts/pi4.sh
@@ -25,7 +25,7 @@ sleep 10
 
 # Launch chromium browser in fullscreen on that page
 SCREEN_SCALE="${SCREEN_SCALE:-1.0}"
-chromium-browser --app=http://localhost:8081 --start-fullscreen --no-sandbox --user-data-dir --kiosk --force-device-scale-factor=$SCREEN_SCALE
+chromium-browser --app=http://localhost:8081 --start-fullscreen --no-sandbox --user-data-dir --kiosk --disable-dev-shm-usage --disable-backing-store-limit --enable-logging=stderr --v=1 --force-device-scale-factor=$SCREEN_SCALE
 
 # For debugging
 echo "Chromium browser exited unexpectedly."
