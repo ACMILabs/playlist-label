c685b6a9cb9cf07887ef55eec3eb80647c17a47a
Sam Maher <sam.maher@acmi.net.au>
2021-06-22 13:07:22 +1000

    #83 Implemented failure dialogue window

--- app/main.py
+++ app/main.py
@@ -301,6 +301,12 @@ def collect_item():
     """
     Collect a tap and forward it on to XOS with the label ID.
     """
+    has_tapped = HasTapped.get_or_none(has_tapped=0)
+    if not has_tapped:
+        raise HTTPError('Tap still processing.')
+    has_tapped.tap_successful = 0
+    has_tapped.has_tapped = 1
+
     xos_tap = dict(request.get_json())
     record = model_to_dict(Message.select().order_by(Message.datetime.desc()).get())
     xos_tap['label'] = record.pop('label_id', None)
@@ -308,15 +314,7 @@ def collect_item():
     headers = {'Authorization': 'Token ' + AUTH_TOKEN}
     response = requests.post(XOS_TAPS_ENDPOINT, json=xos_tap, headers=headers)
 
-    has_tapped = None
-    try:
-        has_tapped = HasTapped.get(has_tapped=1)
-    except:
-        has_tapped = HasTapped.get(has_tapped=0)
-        has_tapped.has_tapped = 1
-
     if response.status_code != requests.codes['created']:
-        has_tapped.tap_successful = 0
         has_tapped.save()
         raise HTTPError('Could not save tap to XOS.')
 
